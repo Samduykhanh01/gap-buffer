1d70fd79feff93a0d198d888ddda7c5633e6caa5
Samduykhanh01 <samduykhanh01@gmail.com>
2024-08-06 16:14:47 -0400

    Removed a.out

---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+  printf("The size of a uint8_t is %lu\n", sizeof(uint8_t));
+  printf("The size of a char is %lu\n", sizeof(char));
+  return 0;
+}
